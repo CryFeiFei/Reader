878b32d45b436b89cc92e16dd387851d68157435
zhangpf <809151882@qq.com>
2016-10-25 23:30:53 +0800

    添加俩按钮，设置最右边比例

--- Reader/mdichild.cpp
+++ Reader/mdichild.cpp
@@ -8,22 +8,21 @@ MdiChild::MdiChild()
     m_ThumbnailWidget = new QWidget();
     m_SemanticTree = new QWidget();
 
+
     m_splitterMain = new QSplitter(Qt::Horizontal, this); //水平分割
+
     m_navigationBar = new QWidget(m_splitterMain);
-
-//    m_navigationBar = new QWidget();
     m_topNBWidget = new QWidget();
-
-
+    //上边水平布局
     QHBoxLayout* NBTopLayout = new QHBoxLayout();
     QPushButton* btn_ok = new QPushButton(QWidget::tr("OK"), m_topNBWidget);
     btn_ok->setDefault(true);
+    btn_ok->setFixedSize(10,10);
     QPushButton* btn_cancel = new QPushButton(QWidget::tr("Cancel"), m_topNBWidget);
+    btn_cancel->setFixedSize(10,10);
+    NBTopLayout->addWidget(btn_ok, Qt::AlignLeft);
     NBTopLayout->addStretch();
-    NBTopLayout->addWidget(btn_ok);
-    NBTopLayout->addStretch();
-    NBTopLayout->addWidget(btn_cancel);
-    NBTopLayout->addStretch();
+    NBTopLayout->addWidget(btn_cancel, Qt::AlignRight);
 
     m_tabWidget = new QTabWidget();
     m_tabWidget->setTabPosition(QTabWidget::West);
@@ -31,44 +30,21 @@ MdiChild::MdiChild()
     m_tabWidget->addTab(m_ThumbnailWidget,"utitle");
     m_tabWidget->addTab(m_SemanticTree,"utitle");
 
+    //垂直分割的布局
     QVBoxLayout* naVLayout = new QVBoxLayout();
     naVLayout->addLayout(NBTopLayout);
     naVLayout->addWidget(m_tabWidget);
-//    naVLayout->addLayout(NBTopLayout);
 
-
-
-
-
-
- //   m_splitterMain = new QSplitter(Qt::Horizontal, this); //水平分割
     m_navigationBar->setLayout(naVLayout);
-//    m_navigationBar->addLayout();
-//    m_navigationBar->layout()
 
-
-    
-
-
-//    QTextEdit *textLeft = new QTextEdit(QObject::tr("左部件"), splitterMain);
-//    textLeft->setAlignment(Qt::AlignCenter);
 
     QTextEdit *textRight = new QTextEdit(QObject::tr("右部件"),m_splitterMain);
     textRight->setAlignment(Qt::AlignCenter);
 
-//    QSplitter *splitterRight = new QSplitter(Qt::Vertical, splitterMain);   //右分割窗口，并以主分割窗口作为父窗口
-//    splitterRight->setOpaqueResize(false);
 
-//    QTextEdit *textUp = new QTextEdit(QObject::tr("上部件"),splitterRight);
-//    textUp->setAlignment(Qt::AlignCenter);
+    m_splitterMain->setStretchFactor(2,6);
 
-//    QTextEdit *textMiddle = new QTextEdit(QObject::tr("中间部件"),splitterRight);
-//    textMiddle->setAlignment(Qt::AlignCenter);
-
-//    QTextEdit *textBottom = new QTextEdit(QObject::tr("底部部件"),splitterRight);
-//    textBottom->setAlignment(Qt::AlignCenter);
-
-    m_splitterMain->setStretchFactor(0,3);
+    m_splitterMain->setAutoFillBackground(true);
 
     m_splitterMain->setWindowTitle(QObject::tr("分割窗口"));
     m_splitterMain->show();
