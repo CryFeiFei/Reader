6db042ff9e5973ec649bea0e4593dbb8665a5594
zhangpf <809151882@qq.com>
2017-02-14 23:29:52 +0800

    封装部分popular接口

--- Reader/PDFkit.cpp
+++ Reader/PDFkit.cpp
@@ -1,6 +1,6 @@
 #include "PDFkit.h"
 
-PDFkit::PDFkit()
+PDFkit::PDFkit():m_pdfPage(NULL),m_document(NULL)
 {
 
 }
@@ -8,3 +8,51 @@ PDFkit::PDFkit()
 PDFkit::~PDFkit()
 {
 }
+
+Poppler::Page* PDFkit::GetPage(int nPageNum)
+{
+    if (m_document == NULL)
+        return NULL;
+
+    Poppler::Page* pPage = NULL;
+    pPage = m_document->page(nPageNum);
+
+    Q_ASSERT(pPage != NULL);
+
+    return pPage;
+}
+
+QImage* PDFkit::GetActruallyPageImage(int nPageNum)
+{
+    if (m_document == NULL)
+        return NULL;
+
+    Poppler::Page*  pPage = NULL;
+    pPage = GetPage(nPageNum);
+    if (pPage == NULL)
+        return NULL;
+
+    QSize size = GetActruallyPageSize(nPageNum);
+
+    QImage imageBuffer = pPage->renderToImage(72, 72, 0, nPageNum, size.width(), size.height());
+
+    return &imageBuffer;
+
+}
+
+QSize PDFkit::GetActruallyPageSize(int nPageNum)
+{
+    QSize size = QSize(0,0);
+    if (m_document == NULL)
+        return size;
+
+    Poppler::Page*  pPage = NULL;
+    pPage = GetPage(nPageNum);
+
+    size = pPage->pageSize();
+
+    return size;
+
+}
+
+
--- Reader/PDFkit.h
+++ Reader/PDFkit.h
@@ -1,6 +1,8 @@
 #ifndef PDFKIT_H
 #define PDFKIT_H
 
+#include <QImage>
+
 #include "_Inclusion/Win32/poppler/poppler-qt5.h"
 #include "Kit.h"
 
@@ -17,6 +19,17 @@ public:
 private:
     Poppler::Page*       m_pdfPage;
     Poppler::Document*   m_document;
+
+public:
+    // 获取某一页的页指针
+    Poppler::Page* GetPage(int nPageNum);
+
+
+    // 获得某一页的真实图像
+    QImage* GetActruallyPageImage(int nPageNum);
+
+    // 获得某一页真实的大小
+    QSize GetActruallyPageSize(int nPageNum);
 
 };
 
