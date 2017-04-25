208f2dc522804571607b080aed76a9fd1485d823
zhangpf <809151882@qq.com>
2017-04-26 00:07:21 +0800

    重构代码

--- Reader/DocViewer.cpp
+++ Reader/DocViewer.cpp
@@ -13,11 +13,6 @@ DocViewer::DocViewer(IChildViewer* childviewer) : m_IChildViewer(childviewer)
     m_pscrollarea->setBackgroundRole(QPalette::Dark);
     m_pscrollarea->setWidget(this);
 
-    QScrollBar* pScrollBar = m_pscrollarea->verticalScrollBar();
-//    pScrollBar->setFixedHeight(size.height() * nNumCount);
-    pScrollBar->setMaximum(size.height() * nNumCount);
-    pScrollBar->setSingleStep(8);
-
 
 }
 
