c70280c76527ba8df316a33fd6cf5c175700ed6e
CryFeiFei <809151882@qq.com>
2018-04-02 23:29:15 +0800

    干掉一个resize

--- Reader/DocWidget.cpp
+++ Reader/DocWidget.cpp
@@ -1,12 +1,17 @@
 #include "DocWidget.h"
+#include <QVBoxLayout>
 
 DocWidget::DocWidget(IChildViewer* childviewer):m_IChildViewer(childviewer)
 {
 	setStyleSheet("background-color:gray");
 
-	m_pScrollArea = new QScrollArea(this);
+	m_pScrollArea = new QScrollArea();
 	m_pDocViewer = new DocViewer(this);
 	m_pScrollArea->setWidget(m_pDocViewer);
+
+	QVBoxLayout* layout = new QVBoxLayout();
+	setLayout(layout);
+	layout->addWidget(m_pScrollArea);
 
 	m_pScrollBar = m_pScrollArea->verticalScrollBar();
 
@@ -19,13 +24,13 @@ DocWidget::~DocWidget()
 
 void DocWidget::resizeEvent(QResizeEvent* e)
 {
-	m_pScrollArea->resize(this->size());
+//	m_pScrollArea->resize(this->size());
 	m_pDocViewer->resize(this->size().width(), m_IChildViewer->getPageHighCount());
 }
 
 void DocWidget::RefreshWindow()
 {
 	update();
-	m_pDocViewer->resize(this->size().width(), m_IChildViewer->getPageHighCount());
+//	m_pDocViewer->resize(this->size().width(), m_IChildViewer->getPageHighCount());
 }
 
