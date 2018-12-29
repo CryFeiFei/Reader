7bd7d277e918c12cd8ee1d9a0499296351a9fd53
zhangpf <809151882@qq.com>
2018-12-30 01:33:50 +0800

    添加函数

--- Reader/shell/kribbontoolbutton.cpp
+++ Reader/shell/kribbontoolbutton.cpp
@@ -44,16 +44,39 @@ void KRibbonToolText::paintEvent(QPaintEvent* event)
 //----------------------------------------------------------
 
 KRibbonToolButton::KRibbonToolButton(QWidget *parent) : QWidget(parent)
-	, m_text(nullptr)
-	, m_Icon(nullptr)
+	, m_textWidget(nullptr)
+	, m_IconWidget(nullptr)
 {
 	setObjectName("KRibbonToolButton");
 	_init();
 }
+
+QString KRibbonToolButton::text()
+{
+	return m_text;
+}
+
+void KRibbonToolButton::setText(QString text)
+{
+	m_text = text;
+}
+
+QPixmap KRibbonToolButton::icon()
+{
+	return m_pixmap;
+}
+
+void KRibbonToolButton::setIcon(QPixmap pixmap)
+{
+	m_pixmap = pixmap;
+}
+
 
 void KRibbonToolButton::_init()
 {
 	QVBoxLayout* mainLayout = new QVBoxLayout(this);
 	mainLayout->setContentsMargins(0, 0, 0, 0);
 
+	//m_text->setText()
+	//
 }
--- Reader/shell/kribbontoolbutton.h
+++ Reader/shell/kribbontoolbutton.h
@@ -45,8 +45,16 @@ private:
 class KRibbonToolButton : public QWidget
 {
 	Q_OBJECT
+	Q_PROPERTY(QString text READ text WRITE setText)
+	Q_PROPERTY(QPixmap pixmap READ icon WRITE setIcon)
 public:
 	KRibbonToolButton(QWidget *parent = nullptr);
+
+public:
+	QString text();
+	void setText(QString text);
+	QPixmap icon();
+	void setIcon(QPixmap pixmap);
 
 signals:
 
@@ -56,8 +64,11 @@ private:
 	void _init();
 
 private:
-	KRibbonToolText* m_text;
-	KRibbonToolIcon* m_Icon;
+	QString m_text;
+	QPixmap m_pixmap;
+
+	KRibbonToolText* m_textWidget;
+	KRibbonToolIcon* m_IconWidget;
 };
 
 #endif // KRIBBONTOOLBUTTON_H
